0d4231cfed280114a3e0e4466f810273f747ab0a
Dmytro <dimabokov5@gmail.com>
2025-02-28 10:31:34 +0000

    Menu input validation added

--- cars.cpp
+++ cars.cpp
@@ -9,6 +9,7 @@
 #include <limits.h>
 #include <list>
 
+//Car structure
 struct Car
 {
     std::string make;
@@ -18,13 +19,16 @@ struct Car
     bool isElectric;
 };
 
-struct minMaxEngineSize 
+//Struct to store data for average release year, the newest car and the oldest car  
+struct minMaxYear
 {
-    Car minEngineCar;
-    Car maxEngineCar;
-    int avgEngineSize;
+    Car minYearCar;
+    Car maxYearCar;
+    int avgYear;
 };
 
+
+//parser function to parse the .csv file line into a Car object 
 void parse(const std::string& line, std::string& make, std::string& model, int& year, double& engineSize, bool& isElectric)
 {
     std::stringstream ss(line);
@@ -41,6 +45,7 @@ void parse(const std::string& line, std::string& make, std::string& model, int& 
     isElectric = (temp == "Electric" || temp == "Yes" || temp == "True");
 }
 
+//Function to read the .csv file
 std::vector<Car> readCarsFromFile(const std::string &filename)
 {
     std::vector<Car> cars;
@@ -63,31 +68,45 @@ std::vector<Car> readCarsFromFile(const std::string &filename)
     return cars;
 }
 
+//function to display the vector of Cars 
 void displayCars(const std::vector<Car>& cars)
 {
-    std::cout << std::left << std::setw(15) << "Make" << std::setw(20) << "Model" << std::setw(10) << "Year" << std::setw(15) << "Engine Size" << std::setw(15) << "Fuel Type" << std::endl;
-    for (const Car& car : cars)
+    if(cars.empty())
+        std::cout << "No cars found\n";
+    else 
     {
-        std::cout << std::left << std::setw(15) << car.make << std::setw(20) << car.model << std::setw(10) << car.year << std::setw(15) << car.engineSize << std::setw(15) << (car.isElectric ? "Electric" : "Gas") << std::endl;
+        std::cout << std::left << std::setw(15) << "Make" << std::setw(20) << "Model" << std::setw(10) << "Year" << std::setw(15) << "Engine Size" << std::setw(15) << "Fuel Type" << std::endl;
+        for (const Car& car : cars)
+        {
+            std::cout << std::left << std::setw(15) << car.make << std::setw(20) << car.model << std::setw(10) << car.year << std::setw(15) << std::fixed << std::setprecision(2) << car.engineSize << std::setw(15) << (car.isElectric ? "Electric" : "Gas") << std::endl;
+        }
     }
 }
 
+//override function that accepts list instead of a vector 
 void displayCars(const std::list<Car>& cars)
 {
-    std::list<Car>::const_iterator it;
-    std::cout << std::left << std::setw(15) << "Make" << std::setw(20) << "Model" << std::setw(10) << "Year" << std::setw(15) << "Engine Size" << std::setw(15) << "Fuel Type" << std::endl;
-
-    for(it = cars.cbegin(); it != cars.cend(); it++)
+    if(cars.empty())
+        std::cout << "No card found\n";
+    else 
     {
-        std::cout << std::left << std::setw(15) << it->make << std::setw(20) << it->model << std::setw(10) << it->year << std::setw(15) << it->engineSize << std::setw(10) << (it->isElectric ? "Electric" : "Gas") << std::endl;
+        std::list<Car>::const_iterator it;
+        std::cout << std::left << std::setw(15) << "Make" << std::setw(20) << "Model" << std::setw(10) << "Year" << std::setw(15) << "Engine Size" << std::setw(15) << "Fuel Type" << std::endl;
+
+        for(it = cars.cbegin(); it != cars.cend(); it++)
+        {
+            std::cout << std::left << std::setw(15) << it->make << std::setw(20) << it->model << std::setw(10) << it->year << std::setw(15) << std::fixed << std::setprecision(2) << it->engineSize << std::setw(10) << (it->isElectric ? "Electric" : "Gas") << std::endl;
+        }
     }
 }
 
+//function to display a single Car
 void displayCar(const Car& car)
 {
     std::cout << "Make: " << car.make << ", Model: " << car.model << ", Year: " << car.year << ", Engine Size: " << car.engineSize <<", Fuel Tyre: "<< (car.isElectric ? "Electric" : "Gas") << std::endl;
 }
 
+//function to find an index of a Car by model (entered by user)
 int findIndexOfCarByModel(const std::vector<Car>& cars, const std::string& model)
 {
     for(int i = 0; i < cars.size(); i++)
@@ -100,6 +119,7 @@ int findIndexOfCarByModel(const std::vector<Car>& cars, const std::string& model
     return -1;
 }
 
+//function to display the amount of cars of each make
 std::map<std::string, int> countCarMakes (const std::vector<Car>& Cars)
 {
     std::map<std::string, int> makes;
@@ -110,84 +130,54 @@ std::map<std::string, int> countCarMakes (const std::vector<Car>& Cars)
     return makes;
 }
 
-std::vector<Car> findCarsByMake(const std::vector<Car>& cars, const std::string& make)
+//find cars by make (entered by the user) 
+void findCarsByMake(const std::vector<Car>& cars, const std::string& make)
 {
     std::vector<Car> carsByMake;
     for (const Car& car : cars)
     {
         if(car.make == make)
-        {
             carsByMake.push_back(car);
-        }
     }
-    return carsByMake;
+    displayCars(carsByMake);
 }
 
-// int avgYear(const std::vector<Car>& cars)
-// {
-//     int sum = 0;
-//     int maxEngine = 0;
-//     Car minEngineCar = Car();
-//     Car maxEngineCar = Car();
-//     int minEngine = INT_MAX;
-
-//     for (const Car& car : cars)
-//     {
-//         if(car.engineSize > maxEngine)
-//         {
-//             maxEngine = car.engineSize;
-//             maxEngineCar = car;
-//         }
-
-//         if(car.engineSize < minEngine)
-//         {
-//             minEngine = car.engineSize;
-//             minEngineCar = car;
-//         }
-            
-//         sum += car.engineSize;
-//     }
-//     std::cout << "\nAverage Car Engime Size: " << sum / cars.size() << "L\n";
-//     std::cout << "\nCar with the smallest engine: ";
-//     displayCar(minEngineCar);
-//     std::cout << "\nCar with the largest engine: ";
-//     displayCar(maxEngineCar);
-//     return sum / cars.size();
-// }
-
-minMaxEngineSize avgEngineSize(const std::vector<Car>& cars)
+//function that returns the average release year for cars and newest/oldest car in the list 
+minMaxYear avgYear(const std::vector<Car>& cars)
 {
-    double sum = 0;
-    double maxEngine = 0;
-    Car minEngineCar = Car();
-    Car maxEngineCar = Car();
-    double minEngine = INT_MAX;
+double sum = 0;
+    double maxYear = 0;
+    double minYear = INT_MAX;
+    Car minYearCar = Car();
+    Car maxYearCar = Car();
 
     for (const Car& car : cars)
     {
-        if(car.engineSize > maxEngine)
+        if(car.year > maxYear)
         {
-            maxEngine = car.engineSize;
-            maxEngineCar = car;
+            maxYear = car.year;
+            maxYearCar = car;
         }
 
-        if(car.engineSize < minEngine)
+        if(car.year < minYear)
         {
-            minEngine = car.engineSize;
-            minEngineCar = car;
+            minYear = car.year;
+            minYearCar = car;
         }
             
-        sum += car.engineSize;
+        sum += car.year;
     }
-    return {minEngineCar, maxEngineCar, sum / cars.size()};
+    return {minYearCar, maxYearCar, sum / cars.size()};
 }
 
+//function to search through the vector of cars and find those that have contain substring entered by the user 
 std::list<Car> findMatch(const std::vector<Car>& cars, const std::string& text)
 {
     std::list<Car> carMatches;
     std::vector<Car>::const_iterator it;
     for(it = cars.cbegin(); it != cars.cend(); it++)
     {
+        //std::string::npos in the context of .find() method means "no match"
         if(it->make.find(text) != std::string::npos || it->model.find(text) != std::string::npos)
         {
             carMatches.push_back(*it);
@@ -197,6 +187,7 @@ std::list<Car> findMatch(const std::vector<Car>& cars, const std::string& text)
     return carMatches;
 }
 
+//funtion to sort the Cars in a descending order based on cars engineSize 
 std::vector<Car> sortDescending(std::vector<Car> cars)
 {
     std::sort(cars.begin(), cars.end(), [](const Car& a, const Car& b) {return a.engineSize > b.engineSize;});
--- main.cpp
+++ main.cpp
@@ -12,82 +12,91 @@ void menu()
     std::map<std::string, int> carMakes;
     std::list<Car> carsByMatch;
     std::vector<Car> descendingCars;
-    minMaxEngineSize minMax = minMaxEngineSize();
+    minMaxYear minMax = minMaxYear();
 
     std::string devider = "----------------------------------------";
 
     do{
-        std::cout << "\n" << devider << std::endl;
-        std::cout << "\n1. Display all cars" << std::endl;
-        std::cout << "2. Find index of a car by model" << std::endl;
-        std::cout << "3. Count car makes" << std::endl;
-        std::cout << "4. Display cars by make" << std::endl;
-        std::cout << "5. Display average year, car with min/max engine size" << std::endl;
-        std::cout << "6. Search a car" << std::endl;
-        std::cout << "7. Sort cars in a descendeing order" << std::endl;
-        std::cout << "8. Exit\n" << std::endl;
-        std::cout << devider << std::endl;
-
-        std::cout << "Enter your choice: ";
-        getline(std::cin, optionStr);
-        option = stoi(optionStr);
-
-        switch (option)
+        try
         {
-        case 1:
-            displayCars(cars);
-            break;
-        case 2:
-            std::cout << "Enter model: ";
-            getline(std::cin, desiredModel);
-            index = findIndexOfCarByModel(cars, desiredModel);
-            if(index != -1)
-            {
-                std::cout << "Car found at index: " << index << std::endl;
-            }
-            else
-            {
-                std::cout << "Car not found" << std::endl;
-            }
-            break;
-        case 3:
-            carMakes = countCarMakes(cars);
-            for (const auto& make : carMakes)
-            {
-                std::cout << make.first << ": " << make.second << std::endl;
-            }
-            break;
-        case 4:
-            std::cout << "Enter make: ";
-            getline(std::cin, desiredMake);
-            displayCars(findCarsByMake(cars, desiredMake));
-            break;
-        case 5:
-            minMax = avgEngineSize(cars);
-            std::cout << "\nAverage Engine Size: " << minMax.avgEngineSize << "L" << std::endl;
-            std::cout << "\nCar with the smallest engine: ";
-            displayCar(minMax.minEngineCar);
-            std::cout << "\nCar with the largest engine: ";
-            displayCar(minMax.maxEngineCar);
+            std::cout << "\n" << devider << std::endl;
+            std::cout << "\n1. Display all cars" << std::endl;
+            std::cout << "2. Find index of a car by model" << std::endl;
+            std::cout << "3. Count car makes" << std::endl;
+            std::cout << "4. Display cars by make" << std::endl;
+            std::cout << "5. Display average release year, newest/oldest car " << std::endl;
+            std::cout << "6. Search a car" << std::endl;
+            std::cout << "7. Sort cars in a descendeing order" << std::endl;
+            std::cout << "8. Exit\n" << std::endl;
+            std::cout << devider << std::endl;
 
-            break;
-        case 6:
-            std::cout << "Enter a part of the car's make or model: ";
-            getline(std::cin, desiredModel);
-            carsByMatch = findMatch(cars, desiredModel);
-            displayCars(carsByMatch);
-            break;
-        case 7:
-            descendingCars = sortDescending(cars);
-            displayCars(descendingCars);
-            break;
-        case 8:
-            std::cout << "Exiting..." << std::endl;
-            break;
-        default:
-            std::cout << "Invalid option" << std::endl;
-            break;
+            std::cout << "Enter your choice: ";
+            getline(std::cin, optionStr);
+            option = stoi(optionStr);
+
+            switch (option)
+            {
+            case 1:
+                displayCars(cars);
+                break;
+            case 2:
+                std::cout << "Enter model: ";
+                getline(std::cin, desiredModel);
+                index = findIndexOfCarByModel(cars, desiredModel);
+                if(index != -1)
+                {
+                    std::cout << "Car found at index: " << index << std::endl;
+                }
+                else
+                {
+                    std::cout << "Car not found" << std::endl;
+                }
+                break;
+            case 3:
+                carMakes = countCarMakes(cars);
+                for (const auto& make : carMakes)
+                {
+                    std::cout << make.first << ": " << make.second << std::endl;
+                }
+                break;
+            case 4:
+                std::cout << "Enter make: ";
+                getline(std::cin, desiredMake);
+                findCarsByMake(cars, desiredMake);
+                break;
+            case 5:
+                minMax = avgYear(cars);
+                std::cout << "\nAverage Release Year: " << minMax.avgYear << std::endl;
+                std::cout << "\nThe Oldest Car: ";
+                displayCar(minMax.minYearCar);
+                std::cout << "\nThe Newest Car: ";
+                displayCar(minMax.maxYearCar);
+
+                break;
+            case 6:
+                std::cout << "Enter a part of the car's make or model: ";
+                getline(std::cin, desiredModel);
+                carsByMatch = findMatch(cars, desiredModel);
+                displayCars(carsByMatch);
+                break;
+            case 7:
+                descendingCars = sortDescending(cars);
+                displayCars(descendingCars);
+                break;
+            case 8:
+                std::cout << "Exiting..." << std::endl;
+                break;
+            default:
+                std::cout << "Invalid option" << std::endl;
+                break;
+            }
         }
+        catch(const std::exception& e)
+        {
+            std::cout<<"Invalid Input, Try Again";
+        }
+        
+        
     } while (option != 8);
     
 }
